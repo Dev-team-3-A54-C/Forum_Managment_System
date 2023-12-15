1381539a1d3edd037f16d908218e8dd405a2ed9e
Nikolay Dobrev <nickndobrev@gmail.com>
2023-12-15 10:32:11 +0200

    Created web(presentation), core(business), data(infrastructure) layers

---
+++ ForumManagmentSystem/ForumManagmentSystem.Core/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ForumManagmentSystem.Core
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ForumManagmentSystem.Infrastructure
+{
+    public class Class1
+    {
+
+    }
+}
