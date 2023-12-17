c463bdf13fc52baa4a9ea8e5fdd66e864eca3cbe
Nikolay Dobrev <nickndobrev@gmail.com>
2023-12-17 18:45:02 +0200

    Created PostDb, ReplyDb, TagDb, UserDb classes and added Repositories and Interceptors folder.

---
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Data/Models/PostDb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumManagmentSystem.Infrastructure.Data.Models
+{
+    internal class PostDb
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+        public Guid UserId { get; set; }
+        public virtual UserDb User { get; set; }
+        public virtual ICollection<ReplyDb> Replies { get; set; } = new HashSet<ReplyDb>();
+    }
+}
