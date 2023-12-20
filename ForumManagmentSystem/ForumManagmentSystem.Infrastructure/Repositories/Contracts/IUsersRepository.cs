ffe612817bf3c948263517c830fb074a5c8cc3e0
Hristian <hristian.kr.nikolov@gmail.com>
2023-12-20 11:39:25 +0200

    Added contracts for Users and Posts

---
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/Contracts/IUsersRepository.cs
@@ -0,0 +1,22 @@
+﻿using ForumManagmentSystem.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumManagmentSystem.Infrastructure.Repositories.Contracts
+{
+    internal interface IUsersRepository
+    {
+        IList<UserDb> GetAll();
+        UserDb GetById(int id);
+        UserDb GetByName(string name);
+        PostDb Create(PostDb newUser); // Register
+        PostDb Update(int id, PostDb beer);
+        bool Delete(int id);
+        bool UserExists(string name);
+        int Count();
+
+    }
+}
