aa8efddb5d106c6c9f58f8f3f630aacf86c4ae69
Nikolay Dobrev <nickndobrev@gmail.com>
2023-12-22 11:14:25 +0200

    Added Automapper and created single profile.

---
+++ ForumManagmentSystem/ForumManagmentSystem.Core/Helpers/MappingConfig/PostProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using ForumManagmentSystem.Core.ResponseDTOs;
+using ForumManagmentSystem.Infrastructure.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumManagmentSystem.Core.Helpers.MappingConfig
+{
+    public class PostProfile : Profile
+    {
+        public PostProfile()
+        {
+            this.CreateMap<PostDb, PostResponseDTO>()
+                .ForMember(d => d.Likes, p => p.MapFrom(s => s.LikesCount));
+        }
+    }
+}
