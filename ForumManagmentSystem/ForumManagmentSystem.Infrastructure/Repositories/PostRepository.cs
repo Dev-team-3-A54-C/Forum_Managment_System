3490d06843a460cae74ecccc9d3a38e6e8a4bf34
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-05 19:42:04 +0200

    Filled IReplyRepository.

--- ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/PostRepository.cs
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/PostRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ForumManagmentSystem.Infrastructure.Repositories
 {
-    internal class PostRepository
+    public class PostRepository
     {
     }
 }
--- ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/ReplyRepository.cs
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/ReplyRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ForumManagmentSystem.Infrastructure.Repositories
 {
-    internal class ReplyRepository
+    public class ReplyRepository
     {
     }
 }
