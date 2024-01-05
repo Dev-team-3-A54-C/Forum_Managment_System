ba1410408c48d9ae201e360382c05bd12bc1d9c7
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-05 19:01:52 +0200

    Added Reply, Post and Tag Repos and Reply and Tag repo interfaces.

---
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/Contracts/ITagRepository.cs
@@ -0,0 +1,13 @@
+﻿using ForumManagmentSystem.Infrastructure.Data.Models;
+
+namespace ForumManagmentSystem.Infrastructure.Repositories.Contracts
+{
+    public interface ITagRepository
+    {
+        TagDb GetByName(string name);
+        IEnumerable<TagDb> GetAll();
+        TagDb Create(string name);
+        TagDb Update(string name);
+        TagDb Delete(string name);
+    }
+}
--- ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/PostRepository.cs
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/PostRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ForumManagmentSystem.Infrastructure.Repositories
 {
-    internal class DeleteLater
+    internal class PostRepository
     {
     }
 }
