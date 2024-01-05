05bd0e524e8a90692d403bde69fac3925fd6528b
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-05 20:36:41 +0200

    Added interface in ReplyRepo.

--- ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/ReplyRepository.cs
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/ReplyRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ForumManagmentSystem.Infrastructure.Data.Models;
+using ForumManagmentSystem.Infrastructure.Repositories.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,41 @@ using System.Threading.Tasks;
 
 namespace ForumManagmentSystem.Infrastructure.Repositories
 {
-    public class ReplyRepository
+    public class ReplyRepository : IReplyRepository
     {
+        public Task<ReplyDb> Create(ReplyDb reply)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ReplyDb> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ReplyDb>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ReplyDb>> GetLikedRepliesFromUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ReplyDb>> GetRepliesFromUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ReplyDb>> PostReplies(string postTitle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ReplyDb> Update(Guid id, ReplyDb newReply)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
