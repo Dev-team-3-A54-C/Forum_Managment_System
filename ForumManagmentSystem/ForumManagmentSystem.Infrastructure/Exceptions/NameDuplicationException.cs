2a03dc0ee6ba88ee29c92325e6547fbaac3deba3
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-07 19:32:11 +0200

    Added NameDuplicationException.

---
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Exceptions/NameDuplicationException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumManagmentSystem.Infrastructure.Exceptions
+{
+    public class NameDuplicationException : ApplicationException
+    {
+        public NameDuplicationException(string message) : base(message) { }        
+            
+    }
+}
