284ee980456604ecb70c7f5021301b7e52bca76c
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-07 19:33:01 +0200

    Implemented Create func in TagService and added 2 automapper profiles.

--- ForumManagmentSystem/ForumManagmentSystem.Core/Services/TagService.cs
+++ ForumManagmentSystem/ForumManagmentSystem.Core/Services/TagService.cs
@@ -1,15 +1,40 @@
-﻿using ForumManagmentSystem.Core.RequestDTOs;
+﻿using AutoMapper;
+using ForumManagmentSystem.Core.RequestDTOs;
 using ForumManagmentSystem.Core.ResponseDTOs;
 using ForumManagmentSystem.Core.Services.Contracts;
+using ForumManagmentSystem.Infrastructure.Data.Models;
+using ForumManagmentSystem.Infrastructure.Exceptions;
+using ForumManagmentSystem.Infrastructure.Repositories.Contracts;
 
 
 namespace ForumManagmentSystem.Core.Services
 {
     public class TagService : ITagService
     {
+        private readonly ITagRepository tagRepository;
+        private readonly IMapper mapper;
+
+        public TagService(ITagRepository tagRepository, IMapper mapper)
+        {
+            this.tagRepository = tagRepository;
+            this.mapper = mapper;
+        }
+
         public TagResponseDTO Create(TagDTO tag)
         {
-            throw new NotImplementedException();
+            if(tagRepository.DoesNameExist(tag.Name).Result)
+            {
+                throw new NameDuplicationException($"Tag with name {tag.Name} already exist.");
+            }
+
+
+            //TODO: Check if automapper works properly
+            var tagToAdd = mapper.Map<TagDb>(tag);
+
+            tagRepository.Create(tagToAdd);
+
+            return mapper.Map<TagResponseDTO>(tagToAdd);
+
         }
 
         public TagResponseDTO DeleteByName(string name)
