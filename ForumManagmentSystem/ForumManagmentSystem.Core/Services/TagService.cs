fb42bd6aeb727b8d8031ded5f107912e80417932
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-07 18:48:09 +0200

    Created ITagService and Reply and TagService.

---
+++ ForumManagmentSystem/ForumManagmentSystem.Core/Services/TagService.cs
@@ -0,0 +1,35 @@
+﻿using ForumManagmentSystem.Core.RequestDTOs;
+using ForumManagmentSystem.Core.ResponseDTOs;
+using ForumManagmentSystem.Core.Services.Contracts;
+
+
+namespace ForumManagmentSystem.Core.Services
+{
+    public class TagService : ITagService
+    {
+        public TagResponseDTO Create(TagDTO tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TagResponseDTO DeleteByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TagResponseDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TagResponseDTO GetByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TagResponseDTO Update(TagDTO tag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
