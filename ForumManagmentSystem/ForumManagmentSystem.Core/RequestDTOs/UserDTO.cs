64f504f3a12edf4e0bcc67e3ec8d77e2103873a2
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-08 21:33:03 +0200

    removed phonenumber from userDTO.

--- ForumManagmentSystem/ForumManagmentSystem.Core/RequestDTOs/UserDTO.cs
+++ ForumManagmentSystem/ForumManagmentSystem.Core/RequestDTOs/UserDTO.cs
@@ -19,8 +19,6 @@ namespace ForumManagmentSystem.Core.RequestDTOs
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
-        [Required]
-        public string PhoneNumber { get; set; }
 
     }
 }
