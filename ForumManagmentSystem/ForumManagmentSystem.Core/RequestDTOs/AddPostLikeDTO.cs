e840542f21134636a9163a57a78480cc8ef848d6
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-08 19:15:27 +0200

    Added AddPostLikeDTO and AddReplyLikeDTO to RequestDTOs. Implemented most members in ReplyService.

---
+++ ForumManagmentSystem/ForumManagmentSystem.Core/RequestDTOs/AddPostLikeDTO.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace ForumManagmentSystem.Core.RequestDTOs
+{
+    public class AddPostLikeDTO
+    {
+        public string PostId { get; set; }
+        public string UserId { get; set; }
+    }
+}
---
+++ ForumManagmentSystem/ForumManagmentSystem.Core/RequestDTOs/AddReplyLikeDTO.cs
@@ -0,0 +1,10 @@
+﻿
+
+namespace ForumManagmentSystem.Core.RequestDTOs
+{
+    public class AddReplyLikeDTO
+    {
+        public string ReplyId { get; set; }
+        public string UserId { get; set; }
+    }
+}
