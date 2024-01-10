aad3468793def284bcf9bd8e83cf16219697ed1d
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-10 12:24:10 +0200

    Added tag deletion with id and title and finish implementing TagController.

--- ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/Contracts/ITagRepository.cs
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/Contracts/ITagRepository.cs
@@ -5,10 +5,12 @@ namespace ForumManagmentSystem.Infrastructure.Repositories.Contracts
     public interface ITagRepository
     {
         Task<TagDb> GetByName(string name);
+        Task<TagDb> GetById(Guid id);
         Task<IEnumerable<TagDb>> GetAll();
         Task<bool> DoesNameExist(string name);
         Task<TagDb> Create(TagDb newTag);
         Task<TagDb> Update(TagDb name);
         Task<TagDb> Delete(string name);
+        Task<TagDb> Delete(Guid id);
     }
 }
