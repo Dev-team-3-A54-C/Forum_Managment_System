d7916c42a0bc7f23081d8572ace4852bee87cd63
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-13 14:35:38 +0200

    Removed unnecceserry usings.

--- ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/Contracts/IPostsRepository.cs
+++ ForumManagmentSystem/ForumManagmentSystem.Infrastructure/Repositories/Contracts/IPostsRepository.cs
@@ -1,9 +1,4 @@
 ﻿using ForumManagmentSystem.Infrastructure.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ForumManagmentSystem.Infrastructure.Repositories.Contracts
 {
