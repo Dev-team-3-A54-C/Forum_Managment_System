c1e10ecf002c853581674f1d7f4eec2d7e72021f
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-13 14:36:27 +0200

    Removed authorization parameter from GetUsers method.

--- ForumManagmentSystem/ForumManagmentSystem.Web/Controllers/API/UsersApiController.cs
+++ ForumManagmentSystem/ForumManagmentSystem.Web/Controllers/API/UsersApiController.cs
@@ -38,10 +38,8 @@ namespace ForumManagmentSystem.Web.Controllers.API
 
         // READ All: Get all Users or filter by parameters
         [HttpGet("")] // api/users/
-        public IActionResult GetUsers([FromHeader] string authorization,[FromQuery] UserQueryParameters filterParameters)
+        public IActionResult GetUsers([FromQuery] UserQueryParameters filterParameters)
         {
-            
-
             IList<UserResponseDTO> users = userService
                 .FilterBy(filterParameters);
 
