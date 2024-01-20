572e4e5b42659e72fb2818bbec8fa2c8918ca718
Nikolay Dobrev <nickndobrev@gmail.com>
2024-01-20 13:41:30 +0200

    Create Unit tests for UserService.Create and added TestHelper class.

---
+++ ForumManagmentSystem/ForummanagmentSystem.Test/TestHelper.cs
@@ -0,0 +1,22 @@
+﻿
+
+using ForumManagmentSystem.Core.RequestDTOs;
+
+namespace ForummanagmentSystem.Test
+{
+    internal class TestHelper
+    {
+        internal static UserDTO GetTestUser()
+        {
+            return new UserDTO()
+            {
+                FirstName = "FirstName",
+                LastName = "LastName",
+                Username = "Tester",
+                Email = "test@mail.com",
+                Password = "password",
+                PhoneNumber = "1234567890"
+            };
+        }
+    }
+}
